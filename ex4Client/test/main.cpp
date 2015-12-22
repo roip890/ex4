147d76780fbb2347e93f95cbfbcbf8914672b0b5
roi <roi@roi-ThinkPad-S1-Yoga>
2015-12-22 20:37:18 +0200

    version1, before testing.

--- ex4Client/test/main.cpp
+++ ex4Client/test/main.cpp
@@ -14,7 +14,22 @@ GTEST_API_ int main(int argc, char* argv[]) {
 	printf("Running main() from main.cpp\n");
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
-	MoviesSystem* ms = new MoviesSystem();
-	(*ms).start();
-	printf("hhhhhhhhh");
+
+
+	TcpConnection UdpCon = new TcpConnection("127.0.0.1", 5555);
+
+	UdpCon.sendToSocket("hello");
+	UdpCon.receiveFromSocket();
+	UdpCon.getBuffer();
+	UdpCon.closeSocket();
+
+	TcpConnection TcpCon = new TcpConnection("127.0.0.1", 5555);
+
+	TcpCon.sendToSocket("hello");
+	TcpCon.receiveFromSocket();
+	TcpCon.getBuffer();
+	TcpCon.closeSocket();
+
+
+    return 0;
 }
